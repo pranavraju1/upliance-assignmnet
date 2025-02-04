353945314b3fd213efdb3dfb10c787119dba8e8d
pranavraju1 <pranavraju2020@gmail.com>
2025-02-05 01:30:30 +0530

    counter created

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 const Counter = () => {
+    const [count, setCount] = useState(0);
+    const handleDecrease = () => {
+        (count<1)? setCount(0):setCount(count - 1);
+    }
   return (
-    <div>
-        Counter
+    <div className='container'>
+        <div className='card bg-light'>
+            <h3 className='text-center'>Count:{count}</h3>
+            <div className='w-100 row'>
+                <div className='col-md-4 d-flex justify-content-center align-itmes-center'>
+                    <button className='btn btn-warning' onClick={handleDecrease}>Decrease</button>
+                </div>
+                <div className='col-md-4 d-flex justify-content-center align-itmes-center'>
+                    <button className='btn btn-dark' onClick={()=>setCount(0)}>Reset</button>
+                </div>
+                <div className='col-md-4 d-flex justify-content-center align-itmes-center'>
+                    <button className='btn btn-success' onClick={()=>setCount(count+1)}>Increase</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
